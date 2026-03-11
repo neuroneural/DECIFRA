8d488276978de6d76f39bf1430eb3819ee0875b9
Pavel Popov <paavali.popov@gmail.com>
2026-03-10 20:22:12 -0400

    added bigger ukb dataset and a script to train bigger models on it

---
+++ scripts/03_batch_pretraining_ukb_exp.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 1
+#SBATCH --mem=25g
+#SBATCH -p qTRDGPUH
+#SBATCH --gres=gpu:A100:1
+#SBATCH -t 1600                     
+#SBATCH -J pretrain_ukb_exp                   
+#SBATCH -D .                        
+#SBATCH -e ./_error/error_exp%A_%a.err  
+#SBATCH -o ./_out/out_exp%A_%a.out    
+#SBATCH -A psy53c17     
+#SBATCH --array=0-79
+
+# 8 configs (4 small + 4 large meanGRU) * 10 runs each = 80 tasks. Indices 0 to 79
+
+sleep 10s
+
+echo $HOSTNAME >&2
+
+# Run the actual job
+source /data/users2/ppopov1/miniconda/bin/activate pile
+
+CONFIGS=(
+    "assets/configs/meanGRU/all_shared.yaml"
+    "assets/configs/meanGRU/all_independent.yaml"
+    "assets/configs/meanGRU/shared_features_independent_dynamics.yaml"
+    "assets/configs/meanGRU/independent_features_shared_dynamics.yaml"
+    "assets/configs/meanGRU/all_shared_large.yaml"
+    "assets/configs/meanGRU/all_independent_large.yaml"
+    "assets/configs/meanGRU/shared_features_independent_dynamics_large.yaml"
+    "assets/configs/meanGRU/independent_features_shared_dynamics_large.yaml"
+)
+
+# Using modulo logic
+config_idx=$(($SLURM_ARRAY_TASK_ID % ${#CONFIGS[@]}))
+# Integer division groups into subsequent trials 
+idx=$(($SLURM_ARRAY_TASK_ID / ${#CONFIGS[@]}))  
+
+selection=${CONFIGS[$config_idx]}
+dataset="ukb_exp"
+
+# Strip the path to find a nice human-readable descriptor to name the folders
+config_basename=$(basename $selection .yaml)
+echo "Running Pretrain meanGRU Config: $config_basename at Run Index: $idx on $dataset"
+
+PYTHONPATH=. python scripts/01_batch_pretraining.py --idx $idx --model meanGRU --dataset $dataset --hp_config $selection --postfix $config_basename
+
+sleep 30s
