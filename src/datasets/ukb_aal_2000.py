323b362f34a49aa5427a63f61c3b7f178fde6cba
Pavel Popov <paavali.popov@gmail.com>
2026-05-27 19:13:59 -0400

    updated aal imports

--- src/datasets/ukb_aal_2000.py
+++ src/datasets/ukb_aal_2000.py
@@ -2,7 +2,7 @@
 
 import os
 import numpy as np
-from src.settings import DATA_ROOT
+from ..settings import DATA_ROOT
 
 def load_data_2000():
     data = np.load(os.path.join(DATA_ROOT, "ukb_aal", "data_2000.npy"))
